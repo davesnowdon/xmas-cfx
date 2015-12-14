e2ade0b03066d43ef732603ac1e07aa45af180b6
Dave Snowdon <dave@davesnowdon.com>
2015-12-14 20:37:40 +0000

    Get/set label text

--- src/xmas_cfx/core.clj
+++ src/xmas_cfx/core.clj
@@ -1,17 +1,22 @@
 (ns xmas-cfx.core
   (:require [clojurefx.factory :refer [compile]]
             [clojurefx.clojurefx :refer [run-now]])
-  (:import [javafx.scene.control Button Label]
+  (:import [javafx.scene.control Button Label TextField]
            [javafx.scene.layout VBox HBox]
            [javafx.stage StageBuilder]
            [javafx.scene Scene]))
 
+(def text-message (compile [TextField {:text "Yo ho ho, Merry Christmas"}]))
+
+(def xmas-msg (compile [Label {:text "I'm Christmas FX"}]))
 
 (def graph (compile [VBox {:id "TopLevelVBox"
                            :children [Label {:text "Hi!"}
-                                      Label {:text "I'm ClojureFX!"}
+                                      xmas-msg
+                                      text-message
                                       HBox {:id "HorizontalBox"
-                                            :children [Button {:text "Alright."}]}]}]))
+                                            :children [Button {:text "Alright."
+                                                               :action (fn [e] (.setText xmas-msg (.getText text-message)))}]}]}]))
 
 (def ui-root (atom nil))
 
@@ -19,7 +24,7 @@
   (run-now
    (doto
        (.build (StageBuilder/create))
-     (.setTitle "Motivations editor")
+     (.setTitle "Christmas Card Builder")
      (.setScene (Scene. (reset! ui-root graph))))))
 
 (defn start []
